99d8955b4c4f0a32dbfcd6a9868b5fc877c166ce
anik08 <anikdutta0810@gmail.com>
2026-01-10 13:05:05 +0530

    feat: restructure dashboard layout to include AllUsers component and improve grid organization

--- src/screens/dashbard/Dashbaord.tsx
+++ src/screens/dashbard/Dashbaord.tsx
@@ -1,11 +1,14 @@
+
 import React from "react";
 import Layout from "../layout/Layout";
 import { DashboardHeader } from "../../components/screens/dashboard/DashboardHeader";
 import { LeftNavigation } from "../../components/screens/dashboard/LeftNavigation";
+
 import { ProjectProgress } from "../../components/screens/dashboard/ProjectProgress";
 import { UpcomingEvent } from "../../components/screens/dashboard/UpcomingEvent";
 import { TimeSchedule } from "../../components/screens/dashboard/TimeSchedule";
 import { TeamControl } from "../../components/screens/dashboard/TeamControl";
+import AllUsers from "../../components/screens/dashboard/AllUsers";
 
 export const Dashboard = () => {
   return (
@@ -16,12 +19,24 @@ export const Dashboard = () => {
 
         {/* Grid */}
         <div className="grid grid-cols-1 gap-5 lg:grid-cols-12">
-          <LeftNavigation />
-        
+          {/* Left Navigation */}
+          <div className="lg:col-span-3">
+            <LeftNavigation />
+          </div>
+
           {/* Center content */}
           <section className="lg:col-span-6 space-y-5">
-            <UpcomingEvent />
-            <ProjectProgress />
+            <AllUsers />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-stretch">
+              {/* Upcoming Event */}
+              <div className="h-full">
+                <UpcomingEvent />
+              </div>
+              {/* Project Progress */}
+              <div className="h-full">
+                <ProjectProgress />
+              </div>
+            </div>
           </section>
 
           {/* Right column */}
@@ -29,20 +44,6 @@ export const Dashboard = () => {
             <TimeSchedule />
             <TeamControl />
           </aside>
-        </div>
-   <div className="lg:col-span-4">
-  <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 items-stretch">
-
-              {/* Row 2 */}
-              <div className="h-full">
-                <TimeSchedule />
-              </div>
-
-              <div className="h-full">
-                <TeamControl />
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </Layout>
